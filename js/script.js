d4ae68420ece202912ad244f0b224161c568def3
Kommoji  Sai Naga Harshitha <kommojiharshitha@gmail.com>
2026-07-29 08:38:46 +0000

    html

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ alert("Welcome to NRIIT Learning Mangement System")
 let heading = document.getElementById ("welcome");
 heading.innerHTML = "welcome Future Software Engineers"
 console.log("Heading element: ", heading)
+let msg = document.getElementById("message")
+msg.innerHTML = "Javascript is Fun"
+console.log("Message element: ", msg)
+function showmessage() {
+    alert("Welcome to NRIIT Learning Management System ")}                                                                               
