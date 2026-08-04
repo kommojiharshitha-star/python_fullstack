f1b939c887dd7bfe005dd056cc38d6b45c9acdb2
Kommoji  Sai Naga Harshitha <kommojiharshitha@gmail.com>
2026-08-04 10:47:45 +0000

    script updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify,request
 
 app = Flask(__name__)
 
@@ -18,8 +18,15 @@ def contact():
 @app.route("/login")
 def login():
     return render_template("login.html")
-@app.route("/register")
+@app.route('/register',method=["POST","GET"])
 def register():
+    if request.method=="POST":
+        name=request.form["name"]
+        email=request.form["email"]
+        password=request.form["password"]
+        dob=request.form["dob"]
+        gender=request.form["gender"]
+        courses=request.form["courses"]
     return render_template("register.html")
 @app.route("/trainers")
 def trainers():
